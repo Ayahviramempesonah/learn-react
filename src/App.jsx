bb6eaf7486f15b65ef1be8775cfb5b33716fdaea
bayusatriapurnomo <bayusatriapurnomo@gmail.com>
2024-09-22 05:10:19 +0000

    formatdocument

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,28 @@ import '/src/App.css'
 
 
 
-  // belajar tic tac toe
+// belajar tic tac toe
 
-  function Square( {value,onSquareClick}){
+function Square({ value, onSquareClick }) {
 
 
-// const [value,setValue]=useState(null);
+  // const [value,setValue]=useState(null);
 
-    // function TombolKlik(){
+  // function TombolKlik(){
 
-    //   setValue("x");
-    // }
+  //   setValue("x");
+  // }
 
-    return(
-      <button  className='square'  onClick={onSquareClick}>
-        {value}
-      </button>
+  return (
+    <button className='square' onClick={onSquareClick}>
+      {value}
+    </button>
 
-    )
+  )
 
-  }
+}
 
-const products =[
+const products = [
   { title: 'Kubis', isFruit: false, id: 1 },
   { title: 'wortel', isFruit: false, id: 1 },
   { title: 'timun', isFruit: true, id: 1 },
@@ -38,26 +38,26 @@ const products =[
 
 
 
-const user={
-name:'maria',
-imageUrl:'https://i.imgur.com/yXOvdOSs.jpg',
-size:50,
+const user = {
+  name: 'maria',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  size: 50,
 
 
 }
 
-function Kata( {count,onClick}){
+function Kata({ count, onClick }) {
   <h2>klik ajah abangkuh {count} </h2>
 
 }
 
 
-function Tombol({count,onClick}){
+function Tombol({ count, onClick }) {
 
-  return(
-<button onClick={onClick}>
-  btton saya di klik {count} kali
-</button>
+  return (
+    <button onClick={onClick}>
+      btton saya di klik {count} kali
+    </button>
 
   )
 
@@ -66,19 +66,19 @@ function Tombol({count,onClick}){
 
 function App() {
   const [count, setCount] = useState(0)
-  const [squares,setSquares]=useState(Array(9).fill(null));
-  const[xisnext,setIsNext]= useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xisnext, setIsNext] = useState(true);
 
-  function HandleTombol(index){
-if( squares[index]){
-return;
+  function HandleTombol(index) {
+    if (squares[index]) {
+      return;
 
-}
+    }
 
 
-    const nextSquare= squares.slice();
+    const nextSquare = squares.slice();
 
-    nextSquare[index] = xisnext ? "X " : "O" ;
+    nextSquare[index] = xisnext ? "X " : "O";
 
 
     setSquares(nextSquare);
@@ -87,8 +87,8 @@ return;
   }
 
 
-  function HandleClick(){
-setCount(count +9);
+  function HandleClick() {
+    setCount(count + 9);
 
   }
 
@@ -108,51 +108,51 @@ setCount(count +9);
     <>
 
 
-    <h1>{user.name}</h1>
-    <img
-    className='avatar'
-    src={user.imageUrl}
-  alt='foto'
-  style={{
-    width:user.size,
-    height:user.size,
-    borderRadius:'50%',
-    objectFit:'cover',
-  }
+      <h1>{user.name}</h1>
+      <img
+        className='avatar'
+        src={user.imageUrl}
+        alt='foto'
+        style={{
+          width: user.size,
+          height: user.size,
+          borderRadius: '50%',
+          objectFit: 'cover',
+        }
 
-  }
-    
-    />
-<button>mybutton</button>
+        }
 
-<ul>{listItems}</ul>
+      />
+      <button>mybutton</button>
 
-<button onClick={HandleClick}>mybutton  {count} kali di klik</button>
+      <ul>{listItems}</ul>
 
-<Tombol count={count} onClick={HandleClick}/>
-<Tombol count={count} onClick={HandleClick}/>
+      <button onClick={HandleClick}>mybutton  {count} kali di klik</button>
 
-<h2 count={count} onClick={HandleClick}/>
+      <Tombol count={count} onClick={HandleClick} />
+      <Tombol count={count} onClick={HandleClick} />
 
-<h2 className="tic-tac">tic-tac</h2> 
+      <h2 count={count} onClick={HandleClick} />
 
-<div className="board-row">
-  <Square value={squares[0]}  onSquareClick={()=>HandleTombol(0)}/>
-  <Square value={squares[1]} onSquareClick={()=>HandleTombol(1)}/>
-  <Square value={squares[2]} onSquareClick={()=>HandleTombol(2)}/>
-</div>
-<div className="board-row">
-  <Square value={squares[3]} onSquareClick={()=>HandleTombol(3)}/>
-  <Square value={squares[4]} onSquareClick={()=>HandleTombol(4)}/>
-  <Square value={squares[5]} onSquareClick={()=>HandleTombol(5)}/>
-</div>
-<div className="board-row">
-  <Square value={squares[6]}  onSquareClick={()=>HandleTombol(6)}/>
-  <Square  value={squares[7]} onSquareClick={()=>HandleTombol(7)}/>
-  <Square value={squares[8]} onSquareClick={()=>HandleTombol(8)}/>
-</div>
+      <h2 className="tic-tac">tic-tac</h2>
 
-         </>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => HandleTombol(0)} />
+        <Square value={squares[1]} onSquareClick={() => HandleTombol(1)} />
+        <Square value={squares[2]} onSquareClick={() => HandleTombol(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => HandleTombol(3)} />
+        <Square value={squares[4]} onSquareClick={() => HandleTombol(4)} />
+        <Square value={squares[5]} onSquareClick={() => HandleTombol(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => HandleTombol(6)} />
+        <Square value={squares[7]} onSquareClick={() => HandleTombol(7)} />
+        <Square value={squares[8]} onSquareClick={() => HandleTombol(8)} />
+      </div>
+
+    </>
   )
 }
 
